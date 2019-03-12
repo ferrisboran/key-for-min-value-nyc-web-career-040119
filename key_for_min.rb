036adb884a2b189c4fcf6c84c310a1551774e877
Ferris Boran <ferris.boran@gmail.com>
2019-03-12 19:48:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  values = []
+  name_hash.each do |key, value|
+    values << value
+  end
+  values.sort
+  name_hash.each do |key, value|
+    if value == values[0]
+      puts key
+    end
+  end
 end
